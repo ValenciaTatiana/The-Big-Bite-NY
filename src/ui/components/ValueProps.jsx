5f8389d436aebe199faf3073e4bff9c9e8c41dcb
ValenciaTatiana <cuadrost31@gmail.com>
2026-04-28 14:27:49 -0500

    New images and values props

--- src/ui/components/ValueProps.jsx
+++ src/ui/components/ValueProps.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
 
 export const ValueProps = () => {
-  return (
-    <section className="value-props">
-        <div className="value-props__container">
-            <div className="value-props__content">
-                <h2 className="value-props__title">No Hacemos galletas.
-                    <span className="value-props__title-highlight">Hacemos momentos.</span>
-                </h2>
-                <p className="value-props__description">Inspiradas en las calles de Nueva York y obsesionadas con el sabor y la calidad, hacemos galletas que se sienten diferentes. Mas grandes, Mejores.</p>
+    return (
+        <section className="value-props">
+            <div className="value-props__container">
+                <div className="value-props__content">
+                    <h2 className="value-props__title">No Hacemos galletas.
+                        <span className="value-props__title-highlight">Hacemos momentos.</span>
+                    </h2>
+                    <p className="value-props__description">Inspiradas en las calles de Nueva York y obsesionadas con el sabor y la calidad, hacemos galletas que se sienten diferentes. Mas grandes, Mejores.</p>
+                </div>
+                <div className="value-props__item">
+                    <div className="value-props__item-icon">
+                        <img src="/public/img/Ingredients.png" alt="Ingredientes reales y de calidad." />
+                        <h3 className="value-props__item-title">Ingredientes reales y de calidad.</h3>
+                    </div>
+
+                    <div className="value-props__item-icon">
+                        <img src="/public/img/Baked daily.png" alt="Ingredientes reales y de calidad." />
+                        <h3 className="value-props__item-title">Ingredientes reales y de calidad.</h3>
+                    </div>
+
+                    <div className="value-props__item-icon">
+                        <img src="/public/img/Smoothness.png" alt="Ingredientes reales y de calidad." />
+                        <h3 className="value-props__item-title">Ingredientes reales y de calidad.</h3>
+                    </div>
+
+                    <div className="value-props__item-icon">
+                        <img src="/public/img/Love.png" alt="Ingredientes reales y de calidad." />
+                        <h3 className="value-props__item-title">Ingredientes reales y de calidad.</h3>
+                    </div>
+                </div>
             </div>
-            <div className="value-props__item">
-                <h3 className="value-props__item-title">La vida es corta</h3>
-                <p className="value-props__item-description">Cómete una galleta</p>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
