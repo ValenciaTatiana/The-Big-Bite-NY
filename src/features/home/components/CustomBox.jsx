f6e0c93e859abadbf6635ca500943546b7b2e0ad
ValenciaTatiana <cuadrost31@gmail.com>
2026-05-06 12:23:16 -0500

    Initial stylized CustomBox component

--- src/features/home/components/CustomBox.jsx
+++ src/features/home/components/CustomBox.jsx
@@ -1,10 +1,15 @@
+import styles from './CustomBox.module.css';
+import customBoxUrl from '../../../shared/assets/images/caja-spanish.png';
 export const CustomBox = () => {
   return (
     <>
-      <section className="custom-box">
-        <div className="custom-box__container">
-          <h2 className="custom-box__title">Mejor Juntas. <span className="custom-box__title-highlight">Arma tu caja.</span></h2>
-          <p className="custom-box__description">¿No puedes decidir? Arma tu caja de 4, 6 o 12 galletas y mezcla tus sabores favoritos.</p>
+      <section className={styles.customBox}>
+        <div className={styles.customBoxContainer}>
+          <div className={styles.customBoxContent}>
+            <h2 className={styles.customBoxTitle}>Mejor Juntas. <span className={styles.customBoxTitleHighlight}>Arma tu caja.</span></h2>
+            <p className={styles.customBoxDescription}>¿No puedes decidir? Arma tu caja de 4, 6 o 12 galletas y mezcla tus sabores favoritos.</p>
+          </div>
+          <img src={customBoxUrl} alt="Caja de galletas" className={styles.customBoxImage} />
         </div>
       </section>
     </>
